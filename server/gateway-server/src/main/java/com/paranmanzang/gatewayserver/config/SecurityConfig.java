7f81d45ead0a1976114b51beb40c1efadf66fa9e
SongJIH <jh2song@naver.com>
2024-10-29 20:56:41 +0900

    add cors url

--- server/gateway-server/src/main/java/com/paranmanzang/gatewayserver/config/SecurityConfig.java
+++ server/gateway-server/src/main/java/com/paranmanzang/gatewayserver/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                 .cors(corsCustomizer -> corsCustomizer
                         .configurationSource(request -> {
                             CorsConfiguration configuration = new CorsConfiguration();
-                            configuration.setAllowedOriginPatterns(Collections.singletonList("https://paranmanzang.com"));
+                            configuration.setAllowedOriginPatterns(Arrays.asList("https://paranmanzang.com", "http://localhost:3000"));
                             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                             configuration.setAllowCredentials(true);
                             configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "nickname"));
