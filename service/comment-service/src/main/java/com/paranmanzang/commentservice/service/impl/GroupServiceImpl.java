fa6f82ccf742ff6ee755f8562f393860c4a4d205
SongJIH <jh2song@naver.com>
2024-10-31 18:42:06 +0900

    update save-joinning

--- service/comment-service/src/main/java/com/paranmanzang/commentservice/service/impl/GroupServiceImpl.java
+++ service/comment-service/src/main/java/com/paranmanzang/commentservice/service/impl/GroupServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,6 +46,7 @@ public class GroupServiceImpl implements GroupService {
                             .enabled(true)
                             .group(savedGroup)
                             .nickname(savedGroup.getNickname())
+                                    .requestAt(LocalDate.now())
                             .build());
 
                     return GroupResponseModel.fromEntity(savedGroup);
